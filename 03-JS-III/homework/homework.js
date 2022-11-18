754d2f56ac74f85d33456ee16930cd297e29a0a0
Fernanda Olivares <folivareslisperguer@gmail.com>
2022-11-18 17:26:55 -0300

    JSIII HW Compleated!

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -7,7 +7,7 @@
 function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
-    // Tu código: REVISAR
+    // Tu código:
     var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     var suma = numeros.reduce(function(acumulador, elemento) {
 	return acumulador + elemento;
@@ -33,17 +33,31 @@ function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  let arrayalcuadrado = [];
+  for(let i = 0; i < array.length ; i++){
+        let elevando = Math.pow(array[i], 2);
+        arrayalcuadrado.push(elevando);
+      }
+    return arrayalcuadrado;
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  let sumadetodosloselemento = 0
+  for(let i = 0; i < array.length ; i++){
+    sumadetodosloselemento+= array[i]
+    };
+    return sumadetodosloselemento;
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  convertirnumastring = num.toString();
+  totaldigitos = convertirnumastring.length;
+  return totaldigitos;
 }
   
   
