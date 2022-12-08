b6bb97fd1fbdfd2976a276b20f336b6f656b1c70
Fernanda Olivares <folivareslisperguer@gmail.com>
2022-12-08 17:11:41 -0300

    HW JSIV 17/17

--- 04-JS-IV/homework/homework.js
+++ 04-JS-IV/homework/homework.js
@@ -132,7 +132,16 @@ function sumarLikesDeUsuario (usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  /*var suma = 0
+  for (let i = 0 ; i < usuario.posts.length; i++) {
+    suma = suma + usuario.posts[i].likes;
+  }
+  return suma;
+  */
+  return usuario.posts.reduce((acum,next) => acum + next.likes, 0);
 }
+
+
 
 function agregarMetodoCalculoDescuento (producto) {
   // Agregar un método (función) al objeto "producto" llamado "calcularPrecioDescuento"
@@ -144,7 +153,10 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-
+  producto.calcularPrecioDescuento = function () {
+    return this.precio - (this.precio * this.porcentajeDeDescuento);
+  }
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
